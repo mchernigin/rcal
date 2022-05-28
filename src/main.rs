2779e8e8c8a4164b30e5bd1a5eea9d1d68ac9ca1
Michael Chernigin <michaelchernigin@gmail.com>
2022-05-28 15:13:11 +0400

    Week number and month choice

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,39 @@
+use std;
+use clap::Parser;
 use chrono::{self, Datelike, DateTime, Local};
 use colored::Colorize;
 
-fn main() {
-    let now = chrono::Local::now();
+/// Simple calendar
+#[derive(Parser, Debug)]
+#[clap(about, long_about = None)]
+struct Args {
+    /// Show weeks numbers
+    #[clap(short, long)]
+    week_number: bool,
 
-    print_month(now, now);
+    /// Show specific month of current year
+    #[clap(short, long)]
+    month: Option<u32>,
 }
 
-fn print_month(date: DateTime<Local>, now: DateTime<Local>) -> () {
+fn main() {
+    let now = chrono::Local::now();
+    let mut date = now;
+    let args = Args::parse();
+
+    match args.month {
+        Some(m) =>
+            match date.with_month(m) {
+                Some(d) => date = d,
+                None => die(format!("Invalid month: {}", m)),
+            },
+        None => (),
+    }
+    print_month(date, now, args.week_number)
+}
+
+fn print_month(date: DateTime<Local>, now: DateTime<Local>, show_week: bool) {
+    let week_col_size = 4;
     let months = [
         "January",
         "February",
@@ -25,25 +51,47 @@ fn print_month(date: DateTime<Local>, now: DateTime<Local>) -> () {
     let cur_month = months[date.month0() as usize];
     let cur_year = date.year();
     let month_and_year = format!("{} {}", cur_month, cur_year);
+    if show_week {
+        print!("{:<1$}", "", week_col_size);
+    }
     println!("{:^20}", month_and_year);
 
-    for day in ["Mo ", "Tu ", "We ", "Th ", "Fr ", "Sa ", "Su"] {
-        print!("{}", day);
+    if show_week {
+        print!("{:<1$}", "", week_col_size);
+    }
+    for day in ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"] {
+        print!("{} ", day);
     }
     println!();
 
     let shift = date.with_day(1).unwrap().weekday() as u32;
     let days_in_month = days_in_month(date.month0(), date.year() as u32);
+    let mut week_number = date.with_day(1).unwrap().iso_week().week();
+    if show_week {
+        print!("{week_number:>2}{:<1$}", "", week_col_size - 2);
+    }
     print!("{:<1$}", "", (shift * 3) as usize);
+
     for day in 1..(days_in_month + 1) {
         let i =  (day + shift) % 7;
         let cell = format!("{day:>2}");
+
         if date.with_day(day).unwrap() == now {
             print!("{}", cell.reversed());
         } else {
             print!("{}", cell);
         }
-        print!("{}", if i % 7 == 0 { "\n" } else { " " });
+
+        if i % 7 == 0 {
+            println!();
+
+            if show_week {
+                week_number = (week_number + 1) % 52;
+                print!("{week_number:>2}{:<1$}", "", week_col_size - 2);
+            }
+        } else {
+            print!(" ");
+        }
     }
     println!()
 }
@@ -66,4 +114,9 @@ fn is_leap(year: u32) -> bool {
     } else {
         false
     }
+}
+
+fn die(msg: String) {
+    eprintln!("Error: {}", msg);
+    std::process::exit(1);
 }
